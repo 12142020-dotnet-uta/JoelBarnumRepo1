37eedf7a8e0d5cbee1ba8ac8de9608d493382941
joelbarnum <joelbarnum@yahoo.com>
2020-12-14 14:59:35 -0800

    myMeassage

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace JoelBarnumRepo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray =  new string[3];
+            for (int i = 0; i < 3; i++){
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("please enter a word");
+            string response = Console.ReadLine();
+            Console.WriteLine($"your response is {response}.");
+            stringArray.SetValue(response,i);
+            }
+            Console.WriteLine(stringArray.ToString());
+        }
+    }
+}
